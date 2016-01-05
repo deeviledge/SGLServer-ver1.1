afbc669e24ff3fd98ce26d24a51bb7051f59b49e
misimaLab <ai.mychronicle07@gmail.com>
2016-01-05 17:45:18 +0900

    subeteno

---
+++ src/sglserver/keyexchange/RoundServer.java
@@ -0,0 +1,55 @@
+package sglserver.keyexchange;
+/**
+ * グループメンバーにラウンド開始を通知する
+ * 
+ * @author nishimura
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+import sglserver.dynamicpeerinformation.DynamicPeerInformation;
+
+public class RoundServer{
+	private Socket 	socket;
+	@SuppressWarnings({ "unused", "rawtypes" })
+	private List	lineList;
+	
+	
+	
+	public RoundServer(int KeyExchange_Port,String[] names,String command,int round) throws IOException{
+		
+		
+		PrintWriter out;
+		
+		// ログインユーザ情報読み込み
+		DynamicPeerInformation dpinfo = new DynamicPeerInformation();
+		
+                
+                
+		for(int i=0;i<names.length;i++){
+			// グループメンバのIPアドレス取得
+			String ip = dpinfo.getIP(names[i]);
+			//　ソケット通信準備
+			socket = new Socket(ip, KeyExchange_Port);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			// ラウンド開始を伝える
+			out.println(command);
+			//次のラウンド数を伝える
+			if(command.equals("roundstart"))out.println(String.valueOf(round));
+                        //コントローラークラス
+                }
+                System.out.println("RoundServerの処理終了");
+                
+                if(command=="roundstart"){//commandがroundstartの時呼ばれ、finishの時は呼ばれない
+                   ThreadController tc=new ThreadController(names[0],names[1]);
+                }
+
+                
+	}
+}
+
---
+++ src/sglserver/keyexchange/ServerThread.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sglserver.keyexchange;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author tatoo
+ */
+public class ServerThread {
+    ServerSocket serversoc;
+    Socket socket;
+    
+    public ServerThread(String ip,int roundport){
+       
+         try {
+                System.out.println("▽▽▽▽▽▽▽▽▽サーバソケットを生成するよ。ポート番号は"+roundport+"だよ▽▽▽▽▽▽▽▽▽▽▽▽");
+                socket = new Socket( ip , roundport );
+                System.out.println("SGLクライアントIP="+ip+"："+socket.getInetAddress()+"との接続完了");//接続先アドレスを返して表示
+                socket.close();
+                System.out.println("△△△△△△△△△接続してソケット閉じるとこまでいったよ。やった～！△△△△△△△△△△△△");
+            } catch (IOException e) {
+                    // TODO 自動生成された catch ブロック
+                    e.printStackTrace();
+                    System.out.println("サーバーソケット生成中になんかあった");
+            }
+    }
+    /*
+    public void SavePK(){
+    
+    
+    }
+    
+    public void XmlFlg(){
+        
+    }
+    */
+}
